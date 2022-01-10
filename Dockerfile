a09e4fd2005eb2146ac9ea8ce486a85f8f0ea15d
Fabrice Nicol <fabrnicol@gmail.com>
2022-01-10 23:11:47 +0100

    Allow later ROTDs to build later git source revision.

--- Dockerfile
+++ Dockerfile
@@ -24,15 +24,15 @@ RUN apt install -y wget git xz-utils build-essential flex bison libtool texinfo 
                    libncurses-dev libxaw7-dev libjpeg-dev libpng-dev libtiff-dev
 # download auxiliary ROTD build, to bootstrap image building
 RUN wget https://github.com/fabnicol/ubuntu4mercury/releases/download/v1.0.1/rotd.tar.xz
-RUN tar xJvf rotd.tar.xz && rm rotd.tar.xz 
+RUN tar xJvf rotd.tar.xz && rm rotd.tar.xz
 RUN echo PATH='/usr/local/mercury-rotd-2022-01-09/bin:$PATH' >> /etc/profile
-RUN wget https://github.com/Mercury-Language/mercury-srcdist/archive/refs/tags/rotd-2022-01-09.tar.gz 
+RUN wget https://github.com/Mercury-Language/mercury-srcdist/archive/refs/tags/rotd-2022-01-09.tar.gz
 RUN tar xzvf rotd-2022-01-09.tar.gz
 RUN echo 'source /etc/profile' >> /root/.bashrc
 # rebuild the same ROTD within the docker image for security-minded users
 # using ENV as sourcing /etc/profile will do only when the image is created, not while building it.
 ENV PATH=/usr/local/mercury-rotd-2022-01-09/bin:$PATH
-RUN cd mercury-srcdist-rotd-2022-01-09 && /bin/bash configure && make install PARALLEL=-j4 
+RUN cd mercury-srcdist-rotd-2022-01-09 && /bin/bash configure && make install PARALLEL=-j4
 RUN rm -rf /mercury-srcdist-rotd-2022-01-09
 # now with this secured ROTD build the Mercury git source
 RUN git clone --shallow-since=2022-01-08 \
@@ -41,15 +41,15 @@ RUN git clone --shallow-since=2022-01-08 \
 # synchronize the ROTD and git source dates otherwise it may not build
 RUN cd /mercury \
    && /bin/bash prepare.sh && /bin/bash configure --disable-most-grades \
-   && make install PARALLEL=-j4 
+   && make install PARALLEL=-j4
 RUN echo PATH='$PATH:/usr/local/mercury-DEV/bin' >> /etc/profile
 RUN rm -rf /mercury
 # do the same for emacs, with Mercury support for etags (source code tagging)
 RUN git clone --depth=1 -b master --single-branch git://git.sv.gnu.org/emacs.git
 RUN cd emacs && /bin/bash autogen.sh \
    && /bin/bash configure --prefix=/usr/local/emacs-DEV \
-   && make install -j4 
-# companion tools, you may add tools to his list below as you wish:   
+   && make install -j4
+# companion tools, you may add tools to his list below as you wish:
 RUN apt install -y vim nano gdb
 # adjust paths and cleanup
 RUN echo PATH='$PATH:/usr/local/emacs-DEV/bin' >> /etc/profile
@@ -68,5 +68,3 @@ PATH0=$PATH \n\
 PATH=/usr/local/mercury-DEV/bin:$PATH mmake "$@" \n\
 PATH=$PATH0' > /usr/local/bin/mmake-dev && chmod +x /usr/local/bin/mmake-dev
 RUN apt -y autoremove --purge && apt -y clean
-
-
--- build.sh
+++ build.sh
@@ -4,16 +4,39 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #!/bin/bash
+
+if [ $# = 1 ]
+then
+    echo "Using git source revision $1"
+    sed "s/06f81f1cf0d339a/$1/g" Dockerfile.in > Dockerfile2
+    sed -i "s/@/2022-01-09/g" Dockerfile2
+    DOCKERFILE="Dockerfile2"
+    REVISION="$1"
+    DATE="2022-01-09"
+elif [ $# = 2 ] && [ "$2" != "06f81f1cf0d339a" ]
+then
+    echo "Using ROTD dated $1 and git source revision $2"
+    sed "s/REVISION/$2/g" Dockerfile.in > Dockerfile2
+    sed -i "s/@/$1/g" Dockerfile2
+    DOCKERFILE="Dockerfile2"
+    REVISION="$2"
+    DATE="$1"
+else
+    DOCKERFILE="Dockerfile"
+    REVISION="06f81f1cf0d339a"
+    DATE="2022-01-09"
+fi
+
 if ! docker version -f '{{.Client.Experimental}}' || ! docker version -f '{{.Server.Experimental}}'
 then
     echo $'{\n  "experimental": true\n}' | sudo tee /etc/docker/daemon.json
@@ -23,26 +46,28 @@ then
     then
         # for those with open-rc
         rc-service docker restart
-    fi    
+    fi
     if ! docker version -f '{{.Client.Experimental}}' || ! docker version -f '{{.Server.Experimental}}'
     then
         echo "Could not use experimental version of Docker. Remove --squash from script and run it again."
         echo "(Also remove the present test!)"
         exit 1
     fi
-fi    
+fi
 
-if docker build --squash --file Dockerfile --tag ubuntu:mercury .
+if docker build --squash --file ${DOCKERFILE} --tag ubuntu:mercury${REVISION} .
 then
-    echo "Docker image was built as ubuntu:mercury"
+    echo "Docker image was built as ubuntu:mercury${REVISION}"
 else
     echo "Docker image creation failed."
     exit 2
 fi
-if docker save ubuntu:mercury -o ubuntu4mercury.tar
+if docker save ubuntu:mercury${REVISION} -o ubuntu4mercury.tar
 then
   if  xz -9 -k -f ubuntu4mercury.tar && gzip -v -f ubuntu4mercury.tar \
-                         && echo b2sum: $(b2sum ubuntu4mercury.tar.xz) > SUMS \
+                         && echo "ROTD DATE: ${DATE}" \
+                         && echo "GIT SOURCE REVISION: ${REVISION}" \
+                         && echo b2sum: $(b2sum ubuntu4mercury.tar.xz) >> SUMS \
                          && echo b2sum: $(b2sum ubuntu4mercury.tar.gz) >> SUMS \
                          && echo sha512sum: $(sha512sum ubuntu4mercury.tar.xz) >> SUMS \
                          && echo sha512sum: $(sha512sum ubuntu4mercury.tar.gz) >> SUMS
