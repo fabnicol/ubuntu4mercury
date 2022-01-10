42e57fcc230235dea0d2206bd81d3ae3e9e0e2ad
Fabrice Nicol <fabrnicol@gmail.com>
2022-01-10 12:51:36 +0100

    Add enforcing of experiment version for Docker.

--- build.sh
+++ build.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
+echo $'{\n  "experimental": true\n}' | sudo tee /etc/docker/daemon.json
+mkdir -p ~/.docker
+echo $'{\n  "experimental": "enabled"\n}' | sudo tee ~/.docker/config.json
+if ! service docker restart
+then
+    # for those with open-rc
+    rc-service docker restart
+fi    
+if ! docker version -f '{{.Client.Experimental}}' || ! docker version -f '{{.Server.Experimental}}'
+then
+    echo "Could not use experimental version of Docker. Remove --squash from script and run it again."
+    exit 1
+fi    
 
 if docker build --squash --file Dockerfile --tag ubuntu:mercury .
 then
     echo "Docker image was built as ubuntu:mercury"
 else
     echo "Docker image creation failed."
+    exit 2
 fi
 if docker save ubuntu:mercury -o ubuntu4mercury.tar
 then
-  if  xz -9 ubuntu4mercury.tar && rm ubuntu4mercury.tar \
+  if  xz -9 -k ubuntu4mercury.tar && gzip -v ubuntu4mercury.tar \
                          && echo b2sum: $(b2sum ubuntu4mercury.tar.xz) > SUMS \
-                         && echo sha512sum: $(sha512sum ubuntu4mercury.tar.xz) >> SUMS
+                         && echo b2sum: $(b2sum ubuntu4mercury.tar.gz) >> SUMS \
+                         && echo sha512sum: $(sha512sum ubuntu4mercury.tar.xz) >> SUMS \
+                         && echo sha512sum: $(sha512sum ubuntu4mercury.tar.gz) >> SUMS
   then
       echo "Compression performed."
   else
       echo "Could not compress the image tarball."
+      exit 3
   fi
 else
     echo "Docker could not save the image to tarball."
+    exit 4
 fi
