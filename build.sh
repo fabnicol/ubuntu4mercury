7d5847dd4a843e347c8257be50bf0374a049e678
Fabrice Nicol <fabrnicol@gmail.com>
2022-01-11 21:11:10 +0100

    Get rid of static Dockerfile and temporary Dockerfile2.
    Replaced by .config.

--- build.sh
+++ build.sh
@@ -15,6 +15,16 @@
 
 #!/bin/bash
 
+DOCKERFILE="Dockerfile"
+MERCURY_REV_TEST="$(grep '^m-rev' .config | cut -f 2 -d\ )"
+ROTD_DATE_TEST="$(grep '^rotd-date' .config | cut -f2 -d\ )"
+
+# ROTDS builds can sometimes crash
+# In .config, a reference revision hash is given for a successful build.
+# If either is left blank, 06f81f1cf0d339a is used in the Mercury 
+# git repository master branch and ROTD used is dated 2022-01-09
+# These parameters in .config can be overridden on command line
+
 if [ $# = 1 ]
 then
     if [ "$1" = "--help" ] 
@@ -46,23 +56,34 @@ then
     fi
     
     echo "Using git source revision $1"
-    sed "s/06f81f1cf0d339a/$1/g" Dockerfile.in > Dockerfile2
-    sed -i "s/-@/-2022-01-09/g" Dockerfile2
-    DOCKERFILE="Dockerfile2"
+    sed "s/${MERCURY_REV}/$1/g" Dockerfile.in > "${DOCKERFILE}"
+    sed -i "s/-@/-${ROTD_DATE}/g" Dockerfile
     REVISION="$1"
-    DATE="2022-01-09"
-elif [ $# = 2 ] && [ "$2" != "06f81f1cf0d339a" ]
+    DATE="${ROTD_DATE}"
+elif [ $# = 2 ] && [ "$2" != "${ROTD_DATE}" ]
 then
     echo "Using ROTD dated $1 and git source revision $2"
-    sed "s/REVISION/$2/g" Dockerfile.in > Dockerfile2
-    sed -i "s/-@/-$1/g" Dockerfile2
-    DOCKERFILE="Dockerfile2"
+    sed "s/REVISION/$2/g" Dockerfile.in > "${DOCKERFILE}"
+    sed -i "s/-@/-$1/g" "${DOCKERFILE}"
     REVISION="$2"
     DATE="$1"
 else
-    DOCKERFILE="Dockerfile"
-    REVISION="06f81f1cf0d339a"
-    DATE="2022-01-09"
+    if [ -z "${MERCURY_REV_TEST}" ] || [ -z "${ROTD_DATE_TEST}" ]
+    then 
+        ROTD_DATE=2022-01-09
+        MERCURY_REV=06f81f1cf0d339a
+    else 
+        ROTD_DATE="${ROTD_DATE_TEST}"
+        MERCURY_REV="${MERCURY_REV_TEST}"
+    fi
+
+    echo "Using ROTD dated ${ROTD_DATE} and source rev. ${MERCURY_REV}"
+
+    sed "s/DATE/${ROTD_DATE}/g" Dockerfile.in > "${DOCKERFILE}"
+    sed "s/REVISION/${MERCURY_REV}/g" Dockerfile.in > "${DOCKERFILE}"
+
+    REVISION="${MERCURY_REV}"
+    DATE="${ROTD_DATE}"
 fi
 
 # Emacs can sometimes crash
